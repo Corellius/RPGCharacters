cf67e242a0fbd99bd91d5c86e12f870019fd459f
feranimum <obengsab@gmail.com>
2014-03-05 09:49:03 +0000

    Finally got the templates working.

---
+++ server.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+#import utils, MySQLdb
+
+app = Flask(__name__)
+
+@app.route('/')
+def mainIndex():
+    return render_template('index.html', selectedMenu='Home')
+
+@app.route('/about')
+def anotherPage():
+    return render_template('about.html', selectedMenu='About')
+  
+@app.route('/team')
+def contact():
+    return render_template('team.html', selectedMenu='Team')
+  
+@app.route('/something')
+def example():
+    return render_template('something.html', selectedMenu='Something')
+  
+@app.route('/page')
+def page():
+    return render_template('page.html', selectedMenu='Page')
+  
+if __name__ == '__main__':
+    app.debug=True
+    app.run(host='0.0.0.0', port=3000)
