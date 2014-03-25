25f5d802cc73b7197c94209425d7253fef730e9c
Juliana Kim <jkim24@mail.umw.edu>
2014-03-25 06:29:26 +0000

    Derp

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ RPGCharacters
 =============
 
 Database listing various pre-made and player-made characters for pen and paper RPGs.
+
+INSTRUCTIONS ON HOW TO INSTALL!!!!
+
+1. Move yourself to the RPGCharacters folder.
+2. Source the sprint.sql into MySQL.
+3. Run the server.py, not the rpgsprint.py.
+4. Enjoy the madness.
--- server.py
+++ server.py
@@ -4,33 +4,59 @@ import utils, MySQLdb
 app = Flask(__name__)
 app.secret_key = 'Zq4oA4Dqq3'
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def mainIndex():
-    return render_template('index.html', selectedMenu='Home')
+  db = utils.db_connect()
+  cur = db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
+  queryType = 'None'
+  rows = []
+	
+  if request.method == 'POST':
+    searchTerm = MySQLdb.escape_string(request.form['search'])
+    query = "SELECT * FROM Star_Wars JOIN Appearance JOIN Ability_Scores ON Star_Wars.ID = Appearance.ID AND Star_Wars.ID = Ability_Scores.ID WHERE Star_Wars.Name LIKE '" + searchTerm + "%'"
+    query2 = "SELECT * FROM Force_Powers WHERE Name LIKE '" + searchTerm + "%'"
+    queryType = 'yes'
+    print (query)
+    cur.execute(query)
+    rows = cur.fetchall()
+    cur.execute(query2)
+    rows2 = cur.fetchall()
+  return render_template('index.html', selectedMenu='Home', results=rows, power=rows2, queryType=queryType)
 
-@app.route('/starwars')
+@app.route('/starwars', methods=['GET', 'POST'])
 def starWars():
   db = utils.db_connect()
   cur = db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
-  query = 'SELECT * from Star_Wars'
+  query = "SELECT * FROM Star_Wars JOIN Appearance JOIN Ability_Scores ON Star_Wars.ID = Appearance.ID AND Star_Wars.ID = Ability_Scores.ID"
   cur.execute(query)
   rows = cur.fetchall()
-  return render_template('starwars.html', Star_Wars=rows, selectedMenu='Star Wars')
-
-
+  if request.method == 'POST':
+    searchTerm = MySQLdb.escape_string(request.form['search'])
+    query = "SELECT * FROM Star_Wars JOIN Appearance JOIN Ability_Scores ON Star_Wars.ID = Appearance.ID AND Star_Wars.ID = Ability_Scores.ID WHERE Star_Wars.Name LIKE '" + searchTerm + "%'"
+    queryType = 'yes'
+    cur.execute(query)
+    rows2 = cur.fetchall()
+  return render_template('starwars.html', Star_Wars=rows, results=row2, selectedMenu='Star Wars')
   
 @app.route('/team')
 def contact():
     return render_template('team.html', selectedMenu='Team')
   
-@app.route('/something')
+@app.route('/something', methods=['GET', 'POST'])
 def forcePowers():
   db = utils.db_connect()
   cur = db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
   query = 'SELECT * from Force_Powers'
   cur.execute(query)
   rows = cur.fetchall()
-  return render_template('something.html', Force_Powers=rows, selectedMenu='Force Powers')
+  if request.method == 'POST':
+    searchTerm = MySQLdb.escape_string(request.form['search'])
+    query = "SELECT * FROM Force_Powers WHERE Name LIKE '" + searchTerm + "%'"
+    queryType = 'yes'
+    print (query)
+    cur.execute(query)
+    rows2 = cur.fetchall()
+  return render_template('something.html', Force_Powers=rows, results=rows2, selectedMenu='Force Powers')
   
 @app.route('/page')
 def page():
